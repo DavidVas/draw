df25d1b6561c5a9032f85d2beeb03b80cc75891d
David Vasilevski <david@Davids-MacBook-Pro.local>
2023-05-21 21:10:43 +0800

    full screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import Canvas from './Canvas'
+import resizeCanvas from './resizeCanvas'
 
 function App() {
-  
   const draw = (ctx, frameCount) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    resizeCanvas(ctx.canvas)
+
     ctx.fillStyle = '#000000'
     ctx.beginPath()
     ctx.arc(50, 100, 25, 0, 2*Math.PI)
     ctx.fill()
     ctx.beginPath()
-    ctx.arc(150, 125, 25, (frameCount*0.005)*2*Math.PI, Math.PI+(frameCount*0.005)*2*Math.PI)
+    ctx.arc(250, 150, 25, (frameCount*0.005)*2*Math.PI, Math.PI+(frameCount*0.005)*2*Math.PI)
     ctx.fill()
   }
   
-  return <Canvas draw={draw} />
+  return <Canvas draw={draw} style={{width: '100%', height: '100%'}}/>
 }
 
 export default App
